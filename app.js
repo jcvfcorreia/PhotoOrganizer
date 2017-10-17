837ef7b681d51d9846075200e338eef8d1a4364b
Your Name <you@example.com>
2017-10-17 22:25:31 +0100

    commit changes

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 var express = require('express');
 var app = express();
 
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+
 app.get("/", function(req, res){
-	res.send("Hello World");
+	res.render("index");
 });
 
 
-app.listen("3000", function(req, res){
+app.listen("3000", function(req, res){	
 	console.log("App Started");
 });
